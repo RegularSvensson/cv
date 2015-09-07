3aeed7d2686d22057a7e21d96061fae02a032734
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-07 10:42:03 +0200

    Add routes for home and about

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module("cvApp", ['ngRoute', 'cvControllers']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		controller: 'HomeController',
+		templateUrl: 'views/home.html'
+	})
+	.when('/about', {
+		controller: 'AboutController',
+		templateUrl: 'views/about.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+}]);
