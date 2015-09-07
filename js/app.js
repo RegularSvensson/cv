a88200e090ff5b05f1d03e14dd412c6cad1db2fb
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-07 10:39:11 +0200

    Add cvControllers to app dependencies

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-var app = angular.module("cvApp", ['ngRoute']);
-
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider
-	.when('/', {
-		controller: 'HomeController'
-		templateUrl: 'home.html'
-	})
-	.when('/about', {
-		controller: 'AboutController',
-		templateUrl: 'about.html'
-	})
-	.otherwise({
-		redirectTo: '/'
-	});
-}]);
+var app = angular.module("cvApp", ['ngRoute', 'cvControllers']);
