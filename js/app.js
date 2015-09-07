2a33172c4ef54c8f066023c61feaef16a8b92526
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-07 09:54:31 +0200

    Clean up app.js code

--- js/app.js
+++ js/app.js
@@ -13,23 +13,4 @@ app.config(['$routeProvider', function($routeProvider) {
 	.otherwise({
 		redirectTo: '/'
 	});
-
 }]);
-
-
-/*
-app.config(function ($routeProvider) {
-	$routeProvider
-	.when('/', {
-		controller: 'HomeController',
-		templateUrl: 'views/home.html'
-	})
-	.when('/about', {
-		controller: 'AboutController',
-		templateUrl: 'views/about.html'
-	})
-	.otherwise({
-		redirectTo: '/'
-	});
-});
-*/
