2cc7d49afcc1664cfde4321e457045f15ef5c1a7
RegularSvensson <elias.svensson.1992@gmail.com>
2015-09-07 10:37:59 +0200

    Add AboutController to cvControllers

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -14,3 +14,11 @@ cvControllers.controller('HomeController', function($scope) {
 		message: 'Hello world!'
 	};
 });
+
+cvControllers.controller('AboutController', function($scope) {
+	$scope.about = {
+		name: 'Elias Svensson',
+		age: 23,
+		education: 'Industrial Engineering, Chalmers Institute of Technology'
+	};
+});
